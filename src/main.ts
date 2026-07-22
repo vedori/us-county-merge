5b9e605314968bc09c1dfd911a98745e3e4f8e75
Phil Oriuwa <88069247+vedori@users.noreply.github.com>
2026-07-22 13:31:28 -0400

    added a hero and changed some css

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import './style.css'
+import hero from './sections/hero.html?raw'
 import countyMap from '../data/svg/map.svg?raw'
 import { setupZoom } from './map'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+${hero}
 <section id="map-canvas">
   ${countyMap}
 </section>
 
-<div id="spacer"></div>
+<div class="spacer"></div>
 `
 
 // Use panzoom
