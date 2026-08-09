5cfc6c387966e14e4b2c075ebe66e85b0fe1941d
vedori <88069247+vedori@users.noreply.github.com>
2026-08-09 07:14:58 -0400

    removed unused file

--- scripts/features/economics/economic-model.ts
+++
@@ -1,4 +0,0 @@
-// TODO: replace AllGdpEntries with a Map <geoId, GdpEntry> 
-// (and maybe it will be an Array of two maps [GdpMap, GroupedGdpMap]
-// HOWEVER: a GroupedGdp doesnt hold "real geoIds" and must be expanded into specialized GdpEntries anyways
-// so it will be [GdpMap, GroupedGdpEntry[]]
