4c9c4bb1a92df663b509e287883bde97347cf043
Phil Oriuwa <88069247+vedori@users.noreply.github.com>
2026-09-11 21:36:20 -0400

    info hover wip
    Can log to console the county properties on pointer hover

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
-import hero from './sections/hero.html?raw'
-import countyMap from '../data/svg/map.svg?raw'
-import { setupZoom } from './utils/zoom'
+import hero from '@sections/hero.html?raw'
+import countyMap from '@data/svg/map.svg?raw'
+import { setupZoom } from '@utils/zoom'
+import { setupInfoHover } from '@utils/infoHover'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 ${hero}
@@ -15,3 +16,7 @@ ${hero}
 // Use panzoom
 const map = document.querySelector<SVGElement>('#map')!
 setupZoom(map)
+
+// Shows info of map on hover/over event
+// Includes county and highway info
+setupInfoHover();
--- src/utils/infoHover.ts
+++ src/utils/infoHover.ts
@@ -1,4 +1,17 @@
-import { GDP_GROUP_CONFIG } from "@shared/gdp-group-config";
+// import { GDP_GROUP_CONFIG } from "@shared/gdp-group-config";
 
-export function setupInfoHover(element: SVGElement) {
+const setupCountyInfoHover = () => {
+  const counties: SVGGElement = document.querySelector("#counties")!;
+
+  counties.addEventListener("pointerover", (event) => {
+    const c: SVGPathElement = event.target!;
+    console.log(c.dataset);
+  });
+
+};
+
+
+export function setupInfoHover() {
+  setupCountyInfoHover();
+
 }
